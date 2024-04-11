e48c715cc02b967fa71b18c80171417e2a59ad25
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2024-04-11 09:48:42 -0300

    1.0

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -9,12 +9,28 @@ import { fetchTabela } from './fetchTabela'
 
 const DashBoard = async ({ searchParams }: HomeProps) => {
   const produtos = await fetchTabela({ searchParams })
+  const estoque = produtos.reduce(
+    (acc, produto) => {
+      acc.total += produto.quantidade
+      return acc
+    },
+    { total: 0 }
+  )
   return (
     <Container>
-      <header className="absolute w-full h-60 lg:-mt-32 -mt-[118px] grid grid-flow-row gap-4 grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
-        <div className="p-4 border-4 border-colors-light-terciaria dark:border-colors-dark-terciaria rounded-xl"></div>
-        <div className="p-4 border-4 border-colors-light-terciaria dark:border-colors-dark-terciaria rounded-xl"></div>
-        <div className="p-4 border-4 border-colors-light-terciaria dark:border-colors-dark-terciaria rounded-xl"></div>
+      <header className="absolute w-full h-60 -mt-[118px] grid grid-flow-row grid-rows-2 gap-6 p-6 grid-cols-4 xl:grid-cols-3 xl:grid-rows-1">
+        <section className="flex items-center justify-center p-4 border-4 max-xl:col-span-2 bg-colors-light-card dark:bg-colors-dark-card border-colors-light-terciaria dark:border-colors-dark-terciaria rounded-xl">
+          <div>
+            <p>Total:</p>
+            <p>{estoque.total}</p>
+          </div>
+        </section>
+        <section className="p-4 border-4 max-xl:col-span-2 bg-colors-light-card dark:bg-colors-dark-card border-colors-light-terciaria dark:border-colors-dark-terciaria rounded-xl">
+          2
+        </section>
+        <section className="p-4 border-4 max-xl:row-start-2 max-xl:col-start-2 max-xl:col-span-2 bg-colors-light-card dark:bg-colors-dark-card border-colors-light-terciaria dark:border-colors-dark-terciaria rounded-xl">
+          3
+        </section>
       </header>
     </Container>
   )
