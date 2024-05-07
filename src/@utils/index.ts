441d8328efa6c3c430fccecbb81f7d16ed9cf27d
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2024-05-07 09:06:52 -0300

    1.0

--- src/@utils/index.ts
+++ src/@utils/index.ts
@@ -1,5 +1,6 @@
 export * from './auth';
 export * from './cookies';
 export * from './get-user-current';
+export * from './getArrayPages';
 export * from './validate-user-permissions';
 export * from './verify-permissions-page';
