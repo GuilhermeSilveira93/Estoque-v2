3733c909e95702256f4fedc31e3fa62853685c0d
Guilherme Silveira <guilherme.padovani93@gmail.com>
2024-09-06 19:13:22 -0300

    1

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,6 @@
 import { getRequestConfig } from 'next-intl/server'
 import { notFound } from 'next/navigation'
-const locales = ['en', 'pt', 'es']
+export const locales = ['en', 'pt', 'es']
 export default getRequestConfig(async ({ locale }) => {
   if (!locales.includes(locale as 'pt' | 'en' | 'es')) notFound()
   return {
